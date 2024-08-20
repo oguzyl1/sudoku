0a97dbc8769b9cab0868862daf2ac7279a7a4ac8
Oguzhan Yildiz <oguzyildiz.3536@gmail.com>
2024-08-20 15:44:53 +0300

    puanlama sisteminde iyileştirmeler yapıldı

--- sudoku-game/src/utils/HandleChange.js
+++ sudoku-game/src/utils/HandleChange.js
@@ -1,15 +1,19 @@
-/*sudoku grid içindeki klavyeden giriş yapmayı sağlayan fonksiyon*/
 // utils/HandleChange.js
 
 import { sudokuControl } from "./SudokuControl.js";
 
-
 export const handleChange = (cells, index, value, setCells, setSkorCount) => {
   const newValue = value.match(/[1-9]/) ? value : "";
   const newCells = [...cells];
+  const oldValue = cells[index].value;
+
   if (sudokuControl(newCells, index, newValue)) {
+    if (newValue !== oldValue) {
+      const oldScore = oldValue ? 5 : 0;
+      const newScore = newValue ? 5 : 0;
+      setSkorCount((prev) => prev + newScore - oldScore);
+    }
     newCells[index] = { ...newCells[index], value: newValue };
-    setSkorCount((prev) => prev+5);
   } else {
     newCells[index] = { ...newCells[index], value: "" };
     alert("Geçersiz sayı!");
--- sudoku-game/src/utils/handleNumberClick.js
+++ sudoku-game/src/utils/handleNumberClick.js
@@ -1,25 +1,39 @@
-/*sudoku grid içindeki numpadden giriş yapmayı sağlayan fonksiyon*/
+// utils/HandleNumberClick.js
 
 import { sudokuControl } from "./SudokuControl.js";
 
-export const handleNumberClick = (number, selectedCell, cells, setCells, setSkorCount) => {
+export const handleNumberClick = (
+  number,
+  selectedCell,
+  cells,
+  setCells,
+  setSkorCount
+) => {
   if (selectedCell !== null) {
     const newCells = [...cells];
+    const oldValue = newCells[selectedCell].value;
 
     if (number === null || number === "") {
       newCells[selectedCell] = { ...newCells[selectedCell], value: "" };
-      setCells(newCells);
+      const oldScore = oldValue ? 5 : 0;
+      setSkorCount((prev) => prev - oldScore);
     } else {
       if (
         !cells[selectedCell].locked &&
         sudokuControl(newCells, selectedCell, number.toString())
       ) {
-        newCells[selectedCell] = { ...newCells[selectedCell], value: number };
-        setCells(newCells);
-        setSkorCount((prev) => prev + 5);
+        const oldScore = oldValue ? 5 : 0;
+        const newScore = number ? 5 : 0;
+        setSkorCount((prev) => prev + newScore - oldScore);
+        newCells[selectedCell] = {
+          ...newCells[selectedCell],
+          value: number.toString(),
+        };
       } else {
         alert("Geçersiz Sayı Girdiniz!");
       }
     }
+
+    setCells(newCells);
   }
 };
